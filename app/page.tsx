dff421fb208f6493a0e8aeac18f76569ede3b903
Vadim-27 <vadim.bulanyi@gmail.com>
2024-09-05 20:29:39 +0200

    fix eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main className="w-[976px] bg-black">
+        <main className='w-[976px] bg-black'>
             <div className="ml-auto mr-auto mt-24 flex w-[420px] flex-col items-center justify-center rounded-md bg-[#151515] p-16">
                 <h1 className="font-poppins mb-16 text-4xl font-semibold text-white">Welcome</h1>
                 <p className="font-poppins leading-25 text-[21px] font-medium text-white">
